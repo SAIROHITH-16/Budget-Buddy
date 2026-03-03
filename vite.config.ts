129c71e7c85a3f7957c61a0ab9df75ec97436e27
SAIROHITH-16 <dachepallysairohith@gmail.com>
2026-03-03 11:05:28 +0530

    chore: increase chunkSizeWarningLimit to 1600kb

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
